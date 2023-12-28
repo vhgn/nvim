a8ca56f1c61f16abb0d15b4c73e7a7100e119900
Vahagn <metavahagn@gmail.com>
2023-12-28 17:31:42 +0400

    feat: add none_ls (null_ls) for prettier formatting

--- init.lua
+++ init.lua
@@ -299,6 +299,21 @@ local plugins = {
 	},
 	"L3MON4D3/LuaSnip",
 	"saadparwaiz1/cmp_luasnip",
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.completion.spell,
+				}
+			})
+		end
+	},
 
 	-- version control
 	{ "lewis6991/gitsigns.nvim",       opts = gitsigns_opts },
