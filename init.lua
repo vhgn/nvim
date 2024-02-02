54f21fbb0334cc527f503901937f1dc775702bf1
Vahagn <metavahagn@gmail.com>
2024-02-02 14:35:59 +0400

    feat: add black for python formatting

--- init.lua
+++ init.lua
@@ -287,7 +287,7 @@ local plugins = {
 					-- null_ls.builtins.formatting.stylua,
 					-- null_ls.builtins.diagnostics.eslint,
 					null_ls.builtins.formatting.prettier,
-					-- null_ls.builtins.completion.spell,
+					null_ls.builtins.formatting.black,
 				},
 			})
 		end,
